2139fe4dddaef443093cec59938d3a6e0dcdc9ce
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-14 18:56:15 +0800

    Delete test directory

--- test/database/setup.rb
+++
@@ -1,6 +0,0 @@
-require "active_record"
-
-ActiveRecord::Base.establish_connection \
-  adapter: "sqlite3", database: ":memory:", migrations_paths: File.expand_path("../dummy/db/migrate", __dir__)
-
-ActiveRecord::Base.connection.migration_context.migrate
